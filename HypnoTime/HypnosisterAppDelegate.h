afb19fc8011cb67ff0c13bdcfb42daa7f20f0d49
Sean Riggle <seanriggle@gmail.com>
2013-04-01 18:05:45 -0400

    Initial Commit

---
+++ HypnoTime/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  HypnoTime
+//
+//  Created by Sean Riggle on 4/1/13.
+//  Copyright (c) 2013 Sean Riggle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by Sean Riggle on 4/1/13.
+//  Copyright (c) 2013 Sean Riggle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnosisterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnosisterAppDelegate class]));
+    }
+}
